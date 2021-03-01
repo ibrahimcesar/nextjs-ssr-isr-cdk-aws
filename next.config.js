3310f9d537cdf049a06c3054a4fce8e4b3df95fa
Ibrahim Cesar <ibrahim.souza@nexojornal.com.br>
2021-03-01 18:33:50 -0300

    my Terry Pratchett to any header

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
+const terryPratchett = {
+            key: 'X-Clacks-Overhead',
+            value: 'GNU Terry Pratchett',
+          }
+
 module.exports = {
-  env: {
-    'X-Clacks-Overhead': 'GNU Terry Pratchett',
+    async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          terryPratchett
+        ],
+      },
+      {
+        source: '/:ditto',
+        headers: [
+          terryPratchett
+        ],
+      },
+      {
+        source: '/_next\/([^\/]+\/?)*',
+        headers: [
+          terryPratchett
+        ],
+      },
+    ]
   },
   images: {
     domains: ['raw.githubusercontent.com'],
