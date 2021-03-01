82697ce7a63e7cbda8424b050933af838f98e975
Ibrahim Cesar <ibrahim.souza@nexojornal.com.br>
2021-03-01 08:41:17 -0300

    the usual homage to Terry Pratchett

--- next.config.js
+++ next.config.js
@@ -2,4 +2,17 @@ module.exports = {
   images: {
     domains: ['raw.githubusercontent.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '*',
+        headers: [
+          {
+            key: 'X-Clacks-Overhead',
+            value: 'GNU Terry Pratchett',
+          }
+        ],
+      },
+    ]
+  },
 }
